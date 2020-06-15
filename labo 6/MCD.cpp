669906d5848e2c54abb795fc04547f47a4c24c23
kevnefeg <00005720@uca.edu.sv>
2020-06-14 19:02:14 -0600

    labo 6

--- labo 6/MCD.cpp
+++ labo 6/MCD.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 
 using namespace std;
-int mcd(int nm1, int nm2)
+//Formula
+    int mcd(int nm1, int nm2)
 
 {
     if (nm1 == 0)
     {
         return nm2;
     }
-return mcd (nm2 % nm1, nm1);
+    return mcd (nm2 % nm1, nm1);
      
 }
 
-int main()
+   int main()
 {
-    int nm1;
-  int nm2;
-  cout<<endl;
-
+//entrada de texto
+   int nm1;
+   int nm2;
+   cout<<endl;
    cout<<"Ingrese primer valor"<<endl;
    cin>>nm1;
 
    cout<<"Ingrese segundo valor"<<endl;
    cin>>nm2;
-
+//salida de texto
    cout<<"El maximo comun divisor es: "<<endl<<mcd(nm1,nm2);
 
-
-
+return 0;
+system("pause");
 }
